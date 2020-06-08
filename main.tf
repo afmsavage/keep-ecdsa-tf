4b048b9c98526ed126bdc956f543db4be17dadee
Antonio Savage <afmsavage@gmail.com>
2020-06-07 21:25:21 -0500

    changed the instance type to the free size

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 
 resource aws_instance "keep-ecdsa" {
   ami             = data.aws_ami.ubuntu.id
-  instance_type   = "t3a.small" # 2x2 instance. $8/mo about.  Change to t3a.micro for a 2x1 or a t2.micro for a free instance (1x1)
+  instance_type   = "t2.micro" # free instance type
   security_groups = [aws_security_group.keep.name, aws_security_group.ssh.name]
   user_data       = templatefile("user_data.sh", { public = var.public, password = var.passwd }) #file("${path.module}/user_data.sh")
   key_name        = var.key_name
